ad493a8a3ff7b61ef3acb7a173e7fb6c8774366a
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-08-16 16:39:05 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const host = '0.0.0.0';
+const port = process.env.PORT || 8000;
+
 const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.json())
+app.use(cors())
 
 app.get('/', function (req, res) {
     return res.json(`oi`)
 })
 
-app.listen(3000)
+app.listen(port, host, () => {
+    console.log(`API funcionando no host:`, host, `e na porta:`, port)
+})
