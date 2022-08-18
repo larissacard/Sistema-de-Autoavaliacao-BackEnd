8707c6d3f99e5e970c9939ba423441216a7cd9f3
Ndav07 <94586597+Ndav07@users.noreply.github.com>
2022-08-18 15:38:18 -0300

    Estrutura para requisições no banco

---
+++ controllers/gets/getPequisa.js
@@ -0,0 +1,10 @@
+const cliente = require("../../server/infra/connection");
+cliente.connect();
+
+function getPequisa(){
+
+}
+
+module.exports = {
+    getPequisa
+}
--- server/server.js
+++ server/server.js
@@ -1,17 +1,19 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-const authRoutes = require('../routes/auth')
+const authRoutes = require('../routes/auth');
+const { route } = require('../routes/auth');
 
 const host = '0.0.0.0';
 const port = process.env.PORT || 8000;
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.json())
-app.use(cors())
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
+app.use(cors());
+app.use(route);
 
 app.use('/auth', authRoutes);
 
