070ade9e8f7b4f28be1de8fb539354615053ec1c
larissacard <larissak.cardosom@gmail.com>
2022-08-24 15:14:54 -0300

    Update resposta.js

--- routes/resposta.js
+++ routes/resposta.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth')
-const pesquisa_controller = require('../controllers/pesquisa')
+const resposta_controller = require('../controllers/pesquisa')
 
 router.get('/resposta', auth, resposta_controller.getAll)
 router.get('/resposta/:id', auth, resposta_controller.getReposta)
