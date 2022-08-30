9a116b4ecf5dd00b55de52b83d1f47f8cccaf52c
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-08-30 14:48:16 -0300

    Update grupo.js

--- controllers/grupo.js
+++ controllers/grupo.js
@@ -22,7 +22,8 @@ exports.getOne = async (req, res, next) => {
         console.log(res)
         return res.status(200).json(res)
     } catch (error) {
-        return res.status(400).json({erro: error})
+        if (error)
+            return res.status(400).json({erro: error})
     }
 }
 
