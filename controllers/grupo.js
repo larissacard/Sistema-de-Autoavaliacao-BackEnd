2eb866580e62b078a39776b00f0d8ffc742e047c
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-08-30 14:45:45 -0300

    Update grupo.js

--- controllers/grupo.js
+++ controllers/grupo.js
@@ -13,14 +13,13 @@ exports.getOne = async (req, res, next) => {
     const id = req.params.id
     try {
         const grupo = await Grupo.getOne(id)
-        console.log(grupo.rows)
         const pessoas = await Grupo.getPessoas(id)
-        console.log(pessoas.rows)
 
         // Montando um Objeto para a resposta
         let res = {}
         res = grupo.rows[0]
         res.pessoas = pessoas.rows
+        console.log(res)
         return res.status(200).json(res)
     } catch (error) {
         return res.status(400).json({erro: error})
