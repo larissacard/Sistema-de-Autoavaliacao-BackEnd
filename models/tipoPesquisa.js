37dd212518bb3dbe75e5fb693f7bd6e89a0271e9
larissacard <larissak.cardosom@gmail.com>
2022-08-30 15:05:21 -0300

    organização tipo de pesquisa

--- models/tipoPesquisa.js
+++ models/tipoPesquisa.js
@@ -7,22 +7,21 @@ module.exports = class TipoPesquisa {
     }
 
     static getAll(){
-        return cliente.query("SELECT * from tipo_pesquisa ORDER BY id");
+        return cliente.query(`SELECT * from tipo_pesquisa ORDER BY id`);
     }
  
     static procuraTipoPesquisa(id) {
-        return cliente.query("SELECT * FROM tipo_pesquisa WHERE id = $1", [id]); 
+        return cliente.query(`SELECT * FROM tipo_pesquisa WHERE id = $1`, [id]); 
     }
 
     static deleteTipoPesquisa(id){
         return cliente.query(`DELETE FROM tipo_pesquisa WHERE id = $1`, [id]);
     }
 
-    static putTipoPesquisa(nome, id){
-        return cliente.query(`UPDATE tipo_pesquisa set nome = $1,
-                            WHERE id = $2`, [nome, id])
+    static putTipoPesquisa(pesquisa, id){
+        return cliente.query(`UPDATE tipo_pesquisa (nome,id)`, [pesquisa.nome, id])
     }
     static postTipoPesquisa(pesquisa) {
-        return cliente.query('INSERT INTO tipo_pesquisa (nome) values ($1)', [pesquisa.nome]);
+        return cliente.query(`INSERT INTO tipo_pesquisa (nome) values ($1)`, [pesquisa.nome]);
     }
 }
