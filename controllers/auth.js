b0a83b74c85595c0e984f6e8a678e0fcc365f23d
cadumeneses <carloseduardoms646@gmail.com>
2022-08-30 15:31:02 -0300

    fix: error to send data

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,11 +3,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const Root = require('../models/Root');
 
-exports.cadastrar = async(req, res, next) => {
-    const errors = validationResult(req);
-
-    if(errors.isEmpty()) return
-
+exports.cadastrar = async (req, res, next) => {
     const nome = req.body.nome;
     const tipo = req.body.tipo;
     const email = req.body.email;
@@ -16,19 +12,19 @@ exports.cadastrar = async(req, res, next) => {
     const foto = req.body.foto;
 
     try {
-        if (tipo == 1){
+        if (tipo == 1) {
             const error = new Error('Deus Já Existe!');
             error.statusCode = 400;
             throw error;
         }
 
         if (tipo !== 3 && !senha) {
-            return res.status(400).json({message: "Insira uma Senha"})
+            return res.status(400).json({ message: "Insira uma Senha" })
         }
 
         const qtd_emails = await Root.procurarRootEmail(email)
         if (qtd_emails.rowCount > 0) {
-            return res.status(400).json({message: "Email já cadastrado"})
+            return res.status(400).json({ message: "Email já cadastrado" })
         }
 
         const criptografaSenha = await bcrypt.hash(senha, 12);
@@ -42,16 +38,22 @@ exports.cadastrar = async(req, res, next) => {
             foto: foto
         }
 
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array()})
+        }
+
         try {
             await Root.adicionaRoot(detalhesUsuario);
         } catch (error) {
             return res.status(400).json(error)
         }
 
-       res.status(201).json({ message: 'Success Registered' });
-        
+        res.status(201).json({ message: 'Success Registered' });
+
     } catch (err) {
-        if(!err.statusCode){
+        if (!err.statusCode) {
             err.statusCode = 500;
         }
         next(err)
@@ -59,15 +61,15 @@ exports.cadastrar = async(req, res, next) => {
 }
 
 
-exports.login = async(req,res,next) => {
+exports.login = async (req, res, next) => {
     const cpf = req.body.cpf;
     const senha = req.body.senha;
 
     const user = await Root.procurarRoot(cpf);
     const guardaUser = user.rows[0];
 
-    if(user.rows.length !== 1){
-        return res.status(404).json({message: `Usuário não encontrado`})
+    if (user.rows.length !== 1) {
+        return res.status(404).json({ message: `Usuário não encontrado` })
     }
 
     if (user.rows[0].tipo == 3) {
@@ -78,21 +80,21 @@ exports.login = async(req,res,next) => {
             },
             process.env.SECRET,
             { expiresIn: '2h' }
-            );
-        return res.status(200).json({message: "Login Finalizado", token: token, userId: guardaUser.id, tipo: 3})
+        );
+        return res.status(200).json({ message: "Login Finalizado", token: token, userId: guardaUser.id, tipo: 3 })
 
     } else {
 
         if (!senha) {
-            return res.status(401).json({message: "Insira uma Senha"})
+            return res.status(401).json({ message: "Insira uma Senha" })
         }
 
         const confirmaSenha = await bcrypt.compare(senha, guardaUser.senha);
-    
-        if(!confirmaSenha){
-            return res.status(400).json({message: `Senha Incorreta`})
+
+        if (!confirmaSenha) {
+            return res.status(400).json({ message: `Senha Incorreta` })
         }
-        
+
         const token = jwt.sign(
             {
                 cpf: guardaUser.cpf,
@@ -101,8 +103,8 @@ exports.login = async(req,res,next) => {
             },
             process.env.SECRET,
             { expiresIn: '2h' }
-            );
-            
-            res.status(200).json({ token: token, userId: guardaUser.id, tipo: guardaUser.tipo })
-        }     
+        );
+
+        res.status(200).json({ token: token, userId: guardaUser.id, tipo: guardaUser.tipo })
+    }
 }
--- routes/auth.js
+++ routes/auth.js
@@ -10,17 +10,13 @@ const authController = require('../controllers/auth');
 
 
 router.post('/cadastrar',
-        body('nome').trim().isEmpty(),
-        body('email').isEmail().withMessage('Email Inválido').custom(async (email)=>{
-            const user = await Root.procurarRoot(email);
-            if(user[0].length > 0){
-                return Promise.reject('Email já existe!')
-            }
-        }).normalizeEmail(),
+    
+        body('nome').trim().not().isEmpty(),
+        body('email').isEmail().withMessage('Email Inválido').normalizeEmail(),
         body('senha').trim().isLength({ min: 7 }),
-        body('tipo').trim().isEmpty(),
-        body('cpf').not().trim().isLength({ min: 11, max: 11 }),
-        body('foto').trim().isEmpty()
+        body('cpf').trim().isLength({ min: 11, max: 11 }),
+        body('foto').trim().not().isEmpty()
+    
     , authController.cadastrar
 )
 
