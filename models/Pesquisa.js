5d2b0b8d024431e2aa28cc508971a8dde8745a7a
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-09-08 15:53:29 -0300

    Update Pesquisa.js

--- models/Pesquisa.js
+++ models/Pesquisa.js
@@ -29,7 +29,7 @@ module.exports = class Pesquisa {
 
     static postPesquisa(pesquisa) {
         return cliente.query(`INSERT INTO pesquisa (titulo, descricao, fk_grupo, fk_tipo_pesquisa, fk_usuario, data_inicio, data_fim)
-                              values ($1, $2, $3, $4, $5) returning id`,
+                              values ($1, $2, $3, $4, $5, $6, $7) returning id`,
                               [pesquisa.titulo, pesquisa.descricao, pesquisa.fk_grupo, pesquisa.fk_tipo_pesquisa, pesquisa.fk_usuario, pesquisa.data_inicio, pesquisa.data_fim]);
     }
 
