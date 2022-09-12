1d9f2fa45bcbe163e2437748c03042ddfaaf45a3
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-09-12 14:33:48 -0300

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { validationResult } = require('express-validator');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const Root = require('../models/Root');
+const { s3Uploadv2 } = require("../../middlewares/s3Service");
 
 exports.cadastrar = async (req, res, next) => {
     const nome = req.body.nome;
