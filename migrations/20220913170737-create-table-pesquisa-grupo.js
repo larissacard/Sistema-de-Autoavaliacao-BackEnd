ee7e33fca362c0720cb33d2eb18f727db80dba06
Bruno do Nascimento de Brito <104685758+Brun0Nasc@users.noreply.github.com>
2022-09-13 15:21:45 -0300

    Create 20220913170737-create-table-pesquisa-grupo.js

---
+++ migrations/20220913170737-create-table-pesquisa-grupo.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(`
+      ALTER TABLE pesquisa DROP COLUMN IF EXISTS fk_grupo,
+      DROP COLUMN IF EXISTS data_inicio, 
+      DROP DOLUMN IF EXISTS data_fim;
+
+      CREATE TABLE IF NOT EXISTS pesquisa_grupo(
+        id serial primary key,
+        fk_pesquisa bigint not null,
+        fk_grupo bigint not null,
+        data_inicio timestamp, 
+        data_fim timestamp,
+        created_at timestamp not null default (now()),
+        updated_at timestamp not null default (now()),
+        constraint pesquisa_id
+          foreign key(fk_pesquisa)
+            references pesquisa(id) on delete set null,
+        constraint grupo_id
+          foreign key(fk_grupo)
+            references grupo(id) on delete set null
+      );
+
+      CREATE TRIGGER set_timestamp
+      BEFORE UPDATE ON pesquisa_grupo
+      FOR EACH ROW
+      EXECUTE PROCEDURE trigger_set_timestamp();
+    `)
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(`
+      DROP TABLE IF EXISTS pesquisa_grupo;
+
+      ALTER TABLE pesquisa ADD COLUMN IF NOT EXISTS fk_grupo,
+      ADD COLUMN IF NOT EXISTS data_inicio, 
+      ADD DOLUMN IF NOT EXISTS data_fim;
+    `)
+  }
+};
