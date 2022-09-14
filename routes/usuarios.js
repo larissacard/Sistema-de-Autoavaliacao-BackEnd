14636ad844cf6a0140eee8bebfa180092b135bc8
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-09-14 16:02:26 -0300

    Update usuarios.js

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -8,6 +8,6 @@ const multer = require("multer");
 router.get('/usuarios', auth, usuario_controller.getAll)
 router.get('/usuarios/:id/pesquisas', auth, usuario_controller.getPesquisas)
 router.delete('/usuarios/:id', auth, usuario_controller.deleteUsuario)
-router.put('/usuarios/:id', auth, multer(multerConfig).single('foto'), usuario_controller.updateUsuario)
+router.put('/usuarios/:id', multer(multerConfig).single('foto'), auth, usuario_controller.updateUsuario)
 
 module.exports = router;
